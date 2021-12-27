6f32ed9b5452e1858cf8e38f2173bd1af249ccc2
gabielarocha21 <gabriela.rocha@mercadolivre.com>
2021-12-27 20:20:57 -0300

    first commit

---
+++ src/main/java/com/gabrielarocha/sistemaloja/SistemalojaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielarocha.sistemaloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemalojaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemalojaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrielarocha/sistemaloja/SistemalojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielarocha.sistemaloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemalojaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
